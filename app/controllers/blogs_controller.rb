a067a99cb1a4e704226ab11ef30dc6126b45cbbf
panna001 <sonar.pocket.0926@gmail.com>
2021-01-08 10:43:17 +0000

    [Finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,6 +21,13 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def update
+    @blog = Blog.find(params[:id])
+    @blog.update(blog_params)
+    redirect_to blog_path()
+  end
+
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
